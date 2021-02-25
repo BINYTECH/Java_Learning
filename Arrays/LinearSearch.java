3d461e8a268acf4413c762dd65d478e1413898c2
BINYTECH <39939885+BINYTECH@users.noreply.github.com>
2021-02-25 22:24:59 +0530

    Update LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,1 +1,31 @@
+/*To search for an element linearly in the array if it's present print the index or else return -1*/
 
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+	Scanner s=new Scanner (System.in);
+	int n=s.nextInt();
+	int arr[]=new int[n];
+	int se=s.nextInt();
+	
+	for(int i=0;i<n;i++)
+	{
+		arr[i]=s.nextInt();
+	}
+	System.out.println(LinearsearcH(arr,se));
+}
+public static int LinearsearcH(int arr[], int x)
+{
+	
+	for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]==x)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+}

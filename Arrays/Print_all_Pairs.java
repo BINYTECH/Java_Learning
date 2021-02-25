6d190bf169b008232dfc4d70ae9355ff85e0d810
BINYTECH <39939885+BINYTECH@users.noreply.github.com>
2021-02-25 22:34:32 +0530

    Update Print_all_Pairs.java

--- Arrays/Print_all_Pairs.java
+++ Arrays/Print_all_Pairs.java
@@ -1,1 +1,22 @@
+/*Print all pairs of the array element 
+(1,4) (1,7) (1,6) (1,5) (4,7) (4,6) (4,5) (7,6) (7,5) (6,5) */
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+	int []arr= {1,4,7,6,5};
+	printPairs(arr);
+	}
+public static void printPairs(int arr[])
+{
+	int n=arr.length;
+	for(int i=0;i<n-1;i++)
+	{
+	for(int j=i+1;j<n;j++)
+		{
+		System.out.println("("+arr[i]+","+arr[j]+")");
+		}
+	}
+}
+}
 

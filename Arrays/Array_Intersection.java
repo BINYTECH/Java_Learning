0e55b1dd969ba232843eac0a97f52a4f163fcebd
BINYTECH <39939885+BINYTECH@users.noreply.github.com>
2021-02-25 22:43:58 +0530

    Update Array_Intersection.java

--- Arrays/Array_Intersection.java
+++ Arrays/Array_Intersection.java
@@ -1,1 +1,28 @@
+/*Array Intersection:- Given two arrays print the common elements present in both the arrays. */
 
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+	int []arr= {7,6,5};
+	int arr2[]= {7,5,8,9,45,78};
+	Intersection(arr,arr2);
+	}
+public static void Intersection(int arr[],int []arr2)
+{
+
+	for(int i=0;i<arr.length;i++)
+	{
+	for(int j=0;j<arr2.length;j++)
+		{
+		if(arr[i]==arr2[j])
+		{
+			System.out.print(arr[i]+" ");
+			arr2[j]=Integer.MIN_VALUE;
+		
+		}
+	  }
+	}
+}
+
+}
